c1f7a3977dd54b59b3470823b9fe5f5808d7ac94
Page-MS <pagems@proton.me>
2024-04-23 12:52:08 +0200

    ajout des comptes

---
+++ Compte/Compte_DATA.sql
@@ -0,0 +1,32 @@
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'Freddy Mercury');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'lau_fst');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'Queen');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'Hayliat');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'Brian May');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'Roger Taylor');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'Gambi');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'Laure Ploix');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'no_gru');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'emmanuel_macron');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'PNL');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'Xavier Guchet');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'Jacques Chirac');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'Xxx_Hater_xxX');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'jump_max');
+INSERT INTO Compte(id, nom)
+VALUES(DEFAULT,'chimpanze_combat');
