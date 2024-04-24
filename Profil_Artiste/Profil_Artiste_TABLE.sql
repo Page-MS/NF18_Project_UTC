d336731d6a2de100f75f6a4643f69821e5083f26
Lauranne Fossat <laurannefossat10@gmail.com>
2024-04-24 20:29:52 +0200

    modification de la bio avec ajout contrainte d'unicité et modif de la dernière contrainte pour que le groupe soit null lorsque le type est différent de profil_artiste_solo

--- Profil_Artiste/Profil_Artiste_TABLE.sql
+++ Profil_Artiste/Profil_Artiste_TABLE.sql
@@ -3,13 +3,13 @@ CREATE TYPE TYPE_PROFIL AS ENUM('Profil_Groupe',
 
 CREATE TABLE Profil_Artiste(
     id integer references Compte(id) PRIMARY KEY,
-    bio varchar(500),
+    bio varchar(500) UNIQUE,
     type TYPE_PROFIL NOT NULL,
     groupe integer references Profil_Artiste(id),
-    pays varchar(30) references Pays(nom),
+    pays varchar(30) references Pays(nom) NOT NULL,
     CONSTRAINT check_solo_or_group CHECK (
         (type = 'Profil_Artiste_Solo')
         OR
-        (type <> 'Profil_Artiste_Solo' AND groupe IS NOT NULL)
+        (type <> 'Profil_Artiste_Solo' AND groupe IS NULL)
     )
 );
