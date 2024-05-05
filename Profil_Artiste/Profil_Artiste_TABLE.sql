f767e723f10ce71b7844d40e285142c039e1813c
Lauranne Fossat <laurannefossat10@gmail.com>
2024-05-05 15:31:26 +0200

    bio not unique

--- Profil_Artiste/Profil_Artiste_TABLE.sql
+++ Profil_Artiste/Profil_Artiste_TABLE.sql
@@ -5,7 +5,7 @@ CREATE TYPE TYPE_PROFIL AS ENUM(
 
 CREATE TABLE Profil_Artiste(
     id integer references Compte(id) PRIMARY KEY,
-    bio varchar(500) UNIQUE,
+    bio varchar(500),
     type TYPE_PROFIL NOT NULL,
     groupe integer references Profil_Artiste(id),
     pays varchar(30) references Pays(nom) NOT NULL,
