807368e61040cc1f60677ed2f6a96ee23ea3321e
KolinMTG <colin.manyri@gmail.com>
2024-06-11 12:47:05 +0200

    ajout de Profil_Artiste et Profil_Utilisateurice

---
+++ Non Relationnel/Profil_Artiste/Profil_Artiste_TABLE.sql
@@ -0,0 +1,20 @@
+DROP TYPE IF EXISTS TYPE_PROFIL ;
+
+CREATE TYPE TYPE_PROFIL AS ENUM(
+    'Profil_Groupe',
+    'Profil_Artiste_Solo',
+    'Profil_Artiste');
+
+CREATE TABLE Profil_Artiste(
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(30) NOT NULL,
+    bio varchar(500),
+    type TYPE_PROFIL NOT NULL,
+    groupe integer references Profil_Artiste(id),
+    pays varchar(30), 
+    CONSTRAINT check_solo_or_group CHECK (
+        (type = 'Profil_Artiste_Solo')
+        OR
+        (type <> 'Profil_Artiste_Solo' AND groupe IS NULL)
+    )
+);
---
+++ Non Relationnel/Profil_Utilisateurice/Profil_Utilisateurice_TABLE.sql
@@ -0,0 +1,12 @@
+DROP TYPE IF EXISTS STATUT ;
+CREATE TYPE  STATUT AS ENUM ('premium', 'regulier');
+
+CREATE TABLE Profil_Utilisateurice(
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(30) NOT NULL,
+    mail VARCHAR(30) UNIQUE NOT NULL,
+    mdp VARCHAR(30) NOT NULL,
+    date_inscription DATE NOT NULL,
+    statut STATUT NOT NULL,
+    genres JSONB,
+);
