5288c84448f741c1509b573226348bdaabb5e189
Lauranne Fossat <laurannefossat10@gmail.com>
2024-06-15 18:26:09 +0200

    View Album avec nb de chanson et durée totale calculée dynamiquement (suppression de l'attribut durée dans la table)

--- Non Relationnel/Album/Album_TABLE.sql
+++ Non Relationnel/Album/Album_TABLE.sql
@@ -2,7 +2,6 @@ CREATE TABLE NR_Album(
     id SERIAL PRIMARY KEY,
     titre VARCHAR(30) NOT NULL,
     annee_de_sortie DATE NOT NULL,
-    duree_totale INTEGER NOT NULL,
     artiste_principal INTEGER REFERENCES NR_Profil_Artiste,
     chansons JSON,
     UNIQUE (titre, artiste_principal)
---
+++ Non Relationnel/Views/Albums_view.sql
@@ -0,0 +1,7 @@
+CREATE VIEW V_Albums
+AS SELECT
+        a.id, a.titre, EXTRACT(YEAR FROM a.annee_de_sortie) AS annee_de_sortie, jsonb_array_length(chansons::jsonb) AS nombre_de_chansons,
+        SUM((c.chanson ->> 'duree')::integer) AS durée_totale
+        FROM NR_Album a, LATERAL jsonb_array_elements(a.chansons::jsonb) AS c(chanson)
+        GROUP BY a.id, a.titre
+        ORDER BY a.id;
