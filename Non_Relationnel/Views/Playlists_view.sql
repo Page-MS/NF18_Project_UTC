042f028977f33977c8d5a1e1c035613a2858b8e5
Lauranne Fossat <laurannefossat10@gmail.com>
2024-06-15 22:15:02 +0200

    resolve conflict

---
+++ Non_Relationnel/Views/Playlists_view.sql
@@ -0,0 +1,10 @@
+CREATE VIEW V_Playlists
+AS SELECT
+        p.id as id_playlist, p.titre, p.description,
+        jsonb_agg(c.chanson) AS chansons_de_l_album,
+        jsonb_array_length(p.chansons::jsonb) AS nombre_de_chansons
+FROM  NR_Playlist p
+    CROSS JOIN LATERAL jsonb_array_elements(p.albums::jsonb) AS ab(album)
+    JOIN NR_Album a ON (ab.album ->> 'id')::INTEGER = a.id
+    CROSS JOIN LATERAL jsonb_array_elements(a.chansons::jsonb) AS c(chanson)
+    GROUP BY p.id;
