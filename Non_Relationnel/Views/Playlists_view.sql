dc4d193e54c648f5c0ab47f7adba6fbc5465aeb3
Lauranne Fossat <laurannefossat10@gmail.com>
2024-06-16 12:37:00 +0200

    view playlist valide

--- Non_Relationnel/Views/Playlists_view.sql
+++ Non_Relationnel/Views/Playlists_view.sql
@@ -1,10 +1,18 @@
 CREATE VIEW V_Playlists
-AS SELECT
-        p.id as id_playlist, p.titre, p.description,
-        jsonb_agg(c.chanson) AS chansons_de_l_album,
-        jsonb_array_length(p.chansons::jsonb) AS nombre_de_chansons
-FROM  NR_Playlist p
-    CROSS JOIN LATERAL jsonb_array_elements(p.albums::jsonb) AS ab(album)
-    JOIN NR_Album a ON (ab.album ->> 'id')::INTEGER = a.id
-    CROSS JOIN LATERAL jsonb_array_elements(a.chansons::jsonb) AS c(chanson)
-    GROUP BY p.id;
+AS SELECT p.id, p.titre, p.description, p.autorisation_acces, u.nom as createurice,
+       jsonb_array_length(jsonb_agg(DISTINCT c.chanson)) AS nb_chansons,
+       SUM((c.chanson ->> 'duree')::integer) AS durée_totale,
+       jsonb_agg(DISTINCT c.chanson) AS chansons
+
+FROM NR_Playlist p JOIN NR_Profil_Utilisateurice u ON u.id = p.createurice
+        LEFT JOIN
+
+        (SELECT p.id , jsonb_agg(c.chanson) as chansons_albums
+        FROM NR_Playlist p, jsonb_array_elements(p.albums::jsonb) AS ab(album)
+        JOIN NR_Album a ON (ab.album ->> 'id')::INTEGER = a.id,
+        jsonb_array_elements(a.chansons::jsonb) AS c(chanson)
+        group by p.id) c_alb
+        ON p.id = c_alb.id,
+        jsonb_array_elements(p.chansons::jsonb || coalesce(c_alb.chansons_albums,
+        '[]'::jsonb)) AS c(chanson)
+        GROUP BY p.id, u.nom;
