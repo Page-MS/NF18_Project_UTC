894b727016b95752f3a37672d4cee1720d48a83e
Lauranne Fossat <laurannefossat10@gmail.com>
2024-06-18 00:46:16 +0200

    petite modif view pour ajouter artiste

--- Non_Relationnel/Views/Albums_view.sql
+++ Non_Relationnel/Views/Albums_view.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE VIEW V_Albums
 AS SELECT
-        a.id, a.titre, EXTRACT(YEAR FROM a.annee_de_sortie) AS annee_de_sortie, jsonb_array_length(chansons::jsonb) AS nombre_de_chansons,
+        a.id, a.titre, p.nom as artiste_principale, EXTRACT(YEAR FROM a.annee_de_sortie) AS annee_de_sortie, jsonb_array_length(chansons::jsonb) AS nombre_de_chansons,
         a.chansons, SUM((c.chanson ->> 'duree')::integer) AS durée_totale
-        FROM NR_Album a, LATERAL jsonb_array_elements(a.chansons::jsonb) AS c(chanson)
-        GROUP BY a.id, a.titre
+        FROM NR_Album a JOIN NR_Profil_Artiste p ON p.id = a.artiste_principal , LATERAL jsonb_array_elements(a.chansons::jsonb) AS c(chanson)
+        GROUP BY a.id, a.titre, p.nom
         ORDER BY a.id;
--- Non_Relationnel/Views/Chanson_view.sql
+++ Non_Relationnel/Views/Chanson_view.sql
@@ -1,9 +1,11 @@
 CREATE OR REPLACE VIEW V_Chansons
 AS SELECT
         a.id as id_album, a.titre as album ,CAST(c->>'id' as INTEGER) as id_chanson,
-        c->>'titre' as titre, CAST(c->>'duree' as INTEGER) as duree,
-        c->>'createurice' as interprète, c->>'genre_musical' as genre,
+        c->>'titre' as titre, p.nom as interprète,
+        CAST(c->>'duree' as INTEGER) as duree,
+        c->>'genre_musical' as genre,
         c->'droit_auteurice'-> 'auteurs' as auteurices,
         c->'droit_auteurice'-> 'compositeurs' as compositeurices,
         c->'droit_auteurice'-> 'éditeurs' as éditeurices
-        FROM NR_Album a, JSON_ARRAY_ELEMENTS(a.chansons) c ;
+        FROM NR_Album a, JSON_ARRAY_ELEMENTS(a.chansons) c
+        LEFT JOIN NR_Profil_Artiste p ON CAST(c->>'createurice' as INTEGER) = p.id;
