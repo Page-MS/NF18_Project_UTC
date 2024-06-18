0ef03c8030281335fd1d06d5cc51a236e7727da8
Lauranne Fossat <laurannefossat10@gmail.com>
2024-06-18 12:00:37 +0200

    modif valide

--- Non_Relationnel/non_relationnel.py
+++ Non_Relationnel/non_relationnel.py
@@ -478,41 +478,6 @@ class Chanson():
             self.cur.execute(update_query, (Json(chansons_list), album))
             print("Donnée modifiée avec succès.")
 
-    # def suppresion(self):
-    #     titre = input("Titre de l'album de la chanson à modifier : ")
-    #     artiste = input("Artiste principal de l'album : ")
-    #     self.cur.execute("SELECT art.id from NR_Profil_Artiste art where nom = %s", (artiste,))
-    #     art = self.cur.fetchone()
-
-    #     while not art:
-    #         print("/!\ Le nom d'artiste renseigné n'appartient pas à la base de donnée.\n")
-    #         artiste = input("Artiste principal de l'album : ")
-    #         self.cur.execute("SELECT art.id from NR_Profil_Artiste art where nom = %s", (artiste,))
-    #         art = self.cur.fetchone()
-
-    #     self.cur.execute("SELECT a.id from NR_Album a where a.artiste_principal=%s and a.titre=%s ", (art[0], titre))
-    #     album = self.cur.fetchone()
-
-    #     if not album:
-    #         print("L'artiste n'interprète aucune album ayant ce nom pour titre.")
-
-    #     else :
-    #         true_exit = False
-    #         while not true_exit :
-    #             chanson_choisit = input("Titre de la chanson à modifier : ")
-    #             self.cur.execute("SELECT chansons FROM NR_Album WHERE id = %s", (album,))
-    #             chansons_list = self.cur.fetchone()[0]
-
-    #     for i, chanson in enumerate(chansons_list):
-    #         if chanson['titre'] == chanson_choisit:
-
-        
-    #             update_query = "UPDATE NR_Album SET chansons = %s::jsonb WHERE id = %s ;"
-
-    #             # Exécution de la requête avec les paramètres sécurisés
-    #             self.cur.execute(update_query, (Json(chansons_list), album))
-    #             print("Donnée modifiée avec succès.")
-
     def suppression(self):
         titre = input("Titre de l'album de la chanson à supprimer : ")
         artiste = input("Artiste principal de l'album : ")
@@ -553,7 +518,6 @@ class Chanson():
                     print("La chanson spécifiée n'a pas été trouvée dans l'album.")
 
 
-
     def consultation(self):
         table = PrettyTable()
         self.cur.execute(open("Views/Chanson_view.sql", "r").read())
@@ -564,9 +528,6 @@ class Chanson():
         data = self.cur.fetchall()
         table.add_rows(data)
         print(table)
-
-    def suppression(self):
-        pass
 
 def creation_table(cur):
     cur.execute(open("Profil_Artiste/Profil_Artiste_TABLE.sql", "r").read())
@@ -657,9 +618,8 @@ def main():
                     if choice == '2':
                         consultation(cur, table)
                     if choice == '3':
-                        suppression(cur,table)
+                        suppression(cur, table)
                     table = 'z'
-
 
             if choice == '*':
                 suppression_bdd(cur)
